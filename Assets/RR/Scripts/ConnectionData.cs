0e6fceb351aafc84fe71fe58c9ec029158265859
Gerasti <stepantelefon890@gmail.com>
2025-05-18 18:21:05 +0300

    data

--- Assets/RR/Scripts/ConnectionData.cs
+++ Assets/RR/Scripts/ConnectionData.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class ConnectionData : MonoBehaviour
 {
-    public float I = 0.0f;
-    public float U = 0.0f;
-    public float R = 1.0f; 
+    public float I = 0.0f;//
+    public float U = 0.0f;//
+    public float R = 1.0f; //
 }
